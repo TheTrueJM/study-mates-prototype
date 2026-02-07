55b5733dc2e730596834d8bc681e9cee09c7d9b0
Bilal(Bill) <bilalabdul20059@gmail.com>
2026-02-07 23:36:13 +1000

    Student is added to the database upon registration

--- backend/app/routes/student.py
+++ backend/app/routes/student.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, session, request, redirect, url_for, send_file, render_template
 import os
+from ..database import db, Student
+
 
 
 student_bp = Blueprint("student", __name__)
@@ -12,10 +14,15 @@ def index():
         name = request.form.get("name") # Generate Random on Frontend, Here, or Socket-Server?
         session["tutorial_code"] = code
         session["name"] = name
+        student = Student(tutorial_code=code, username=name)
+        db.session.add(student)
+        db.session.commit()
+        session["student_id"] = student.id
         return redirect(url_for("student.details"))
 
     client_path = os.path.join(os.getcwd(), "../frontend/public/student/index.html")
     return send_file(client_path)
+
 
 
 @student_bp.route("/details", methods=["GET", "POST"])
@@ -27,8 +34,12 @@ def details():
         session["currentGPA"] = request.form.get("currentGPA")
         session["goalGPA"] = request.form.get("goalGPA", 4)
         session["availability"] = request.form.get("availability")
+        student = Student.query.get(session["student_id"])
+        student.currentGPA = float(session["currentGPA"])
+        student.goalGPA = float(session["goalGPA"])
+        db.session.commit()
         return redirect(url_for("student.tutorial", code=session["tutorial_code"]))
-
+    
     client_path = os.path.join(os.getcwd(), "../frontend/public/student/details.html")
     return send_file(client_path)
 
