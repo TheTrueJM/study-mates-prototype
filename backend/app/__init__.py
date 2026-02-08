362ad309f876d3b143a4f0a114dec0f9de9caa49
Jush <36951064+JushBJJ@users.noreply.github.com>
2026-02-08 14:22:52 +0200

    Add timer (#21)
    * add timer for tutorials
    
    * fix bugs
    
    * cleanup repetitive statements and allow staff to set timer before starting discussion
    
    * fix error
    
    * refactor timer functions and fix timer-related bugs

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -6,15 +6,72 @@ from .database import db, Staff
 from .routes import staff_bp, student_bp
 
 import os
+import threading
+from threading import Lock
 
 socketio = SocketIO(
     logger=True,
-    cors_allows_origins = "*",
-    cors_credentials = False
+    cors_allows_origins="*",
+    cors_credentials=False
 )
+
+timer_threads = dict()
+timer_locks = dict()
+app_context_holder = None
+
+
+def _start_timer_thread(code):
+    if code in timer_threads and timer_threads[code].is_alive():
+        return
+
+    timer_locks.setdefault(code, Lock())
+
+    def timer_loop():
+        from time import sleep
+
+        while True:
+            sleep(10)
+
+            if not app_context_holder:
+                continue
+
+            with app_context_holder.app_context():
+                from .sockets import utils
+
+                lock = timer_locks.get(code)
+                if not lock:
+                    return
+
+                with lock:
+                    tutorial = utils.tutorials.get(code)
+                    if not tutorial or not tutorial.get("timer", {}).get("running", False):
+                        return
+
+                    timer = tutorial["timer"]
+                    timer["remaining"] = max(0, timer["remaining"] - 10)
+
+                    if timer["remaining"] == 0:
+                        timer["running"] = False
+                        message = {"message": "Time's up!"}
+                        socketio.emit("timer_notification", message, room=code, namespace="/")
+                        socketio.emit("timer_notification", message, room=code, namespace="/staff")
+                    else:
+                        utils._emit_tutorial_update(code)
+
+    timer_threads[code] = threading.Thread(target=timer_loop, daemon=True)
+    timer_threads[code].start()
+
+
+def _stop_timer_thread(code):
+    timer_threads.pop(code, None)
+    timer_locks.pop(code, None)
+
 
 def create_app():
     app = Flask(__name__)
+
+    global app_context_holder
+    app_context_holder = app
 
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "insecure-key")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///study_mates.sqlite")
