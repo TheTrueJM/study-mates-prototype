04c7a70a2042cd3bafa86e6e3ccf737e0389ee80
TheTrueJM <161416439+TheTrueJM@users.noreply.github.com>
2026-02-28 22:47:47 +1000

    Added About Us and Reworked Feedback

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 // Navbar - Top navigation bar shown on every page
 // Left: site name (links to home), Right: Staff login button
 
+import { useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
+import Button from './Button';
+import Modal from './Modal';
 import { useAuth } from "../hooks/useAuth";
+import QRCode from '../static/Feedback-Form-QR.svg';
 
 function Navbar() {
   const { staffAuth, _ } = useAuth();
   const location = useLocation();
+
+  const [showAboutModal, setShowAboutModal] = useState(false);
 
 
   const showStaffLogin = location.pathname === "/";
@@ -17,7 +23,6 @@ function Navbar() {
 
   return (
     <nav className="nav">
-      
       <div className="nav-inner">
         {onTutorial ? (
           <span className="nav-brand">Study Mates</span> // Not clickable
@@ -28,14 +33,7 @@ function Navbar() {
         )}
           
         <div className="nav-actions">
-          <a
-            href="https://forms.office.com/r/4x1A33ZKUX"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="nav-action"
-          >
-            Give Feedback
-          </a>
+          <a className="nav-action" onClick={() => setShowAboutModal(true)}>About Us</a>
 
           {showStaffLogin && (
             <Link to="/staff/login" className="nav-action">
@@ -49,6 +47,24 @@ function Navbar() {
           )}
         </div>
       </div>
+
+      {showAboutModal && (
+        <Modal title="Study Mates - About Us" onClose={() => setShowAboutModal(false)}>
+          <div className="mb-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, dignissimos repellat aspernatur, harum maxime rem similique nesciunt voluptate fuga numquam dolorum dolores, quos error excepturi necessitatibus veritatis laboriosam dicta ipsam.
+            <br/><br/>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, dignissimos repellat aspernatur, harum maxime rem similique nesciunt voluptate fuga numquam dolorum dolores, quos error excepturi necessitatibus veritatis laboriosam dicta ipsam.
+          </div>
+          <div className="divider flex gap-md justify-center">
+            <div className="qr-code-container">
+              <img src={QRCode} alt="Tutorial QR Code" className="qr-image" />
+            </div>
+            <a href="https://forms.office.com/r/4x1A33ZKUX" target="_blank" rel="noopener noreferrer" className="self-center">
+              <Button> Share Feedback </Button>
+            </a>
+          </div>
+        </Modal>
+      )}
     </nav>
   );
 }
